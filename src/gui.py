e11e883757dfabcfc5550cd5fd0d8ce5d8d6a0da
Carson Swoveland <sciencedude2003@gmail.com>
2025-03-28 13:23:46 -0400

    Remove config from git and instead populate from defaults

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
 import json
 import os
 import queue
+import shutil
 import time
 
 import tkinter
@@ -1708,8 +1709,14 @@ class LithographerGui:
 
 
 def main():
-    with open("config.toml", "rb") as f:
-        config = toml.load(f)
+    try:
+        with open("config.toml", "rb") as f:
+            config = toml.load(f)
+    except FileNotFoundError:
+        print("config.toml does not exist, copying settings from default.toml")
+        shutil.copy("default.toml", "config.toml")
+        with open("config.toml", "rb") as f:
+            config = toml.load(f)
 
     stage_config = config["stage"]
     if stage_config["enabled"]:
