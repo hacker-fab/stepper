e89c56c07c2402824b05bbd60e4ba1f381c6fdfd
Justin Wang <justinwang46@gmail.com>
2025-05-01 15:35:11 -0400

    remove unused alignment code

--- src/stage_control/auto/savesysimage.jl
+++
@@ -1,6 +0,0 @@
-using Pkg
-ENV["PYTHON"] = abspath("C:\\Users\\Hacker Fab\\AppData\\Local\\Programs\\Python\\Python310\\python.exe")
-Pkg.build("PyCall")
-using PackageCompiler
-PackageCompiler.create_sysimage(; sysimage_path="JuliaSysimage.so",
-                                     precompile_execution_file="stage_simulate.jl")
