8a9af3cabba4e3a59584898bdcdaf0b5971ec17a
Azure <bill@bingodeMacBook-Pro.local>
2025-12-04 14:01:31 -0500

    fi

--- src/gui.py
+++ src/gui.py
@@ -2316,9 +2316,11 @@ class ProjectorDisplayFrame:
         self.label.configure(image=self.photo)
 
 class TilingCheckFrame:
-    def __init__(self, parent, event_dispatcher: EventDispatcher):
+    def __init__(self, parent, event_dispatcher: EventDispatcher, img_w, img_h):
         self.frame = ttk.Frame(parent)
         self.event_dispatcher = event_dispatcher
+        self.img_w = img_w
+        self.img_h = img_h
 
         self.capture_button = ttk.Button(
             self.frame, 
@@ -2334,8 +2336,8 @@ class TilingCheckFrame:
         self.frame.columnconfigure(0, weight=1)
 
     def capture_and_stitch(self):
-        self.img = self.event_dispatcher.pattern_image
-        self.img_w, self.img_h = self.img.size # 3840*2, 2160*2 # in pixels 
+        # self.img = self.event_dispatcher.pattern_image
+        # self.img_w, self.img_h = self.img.size # 3840*2, 2160*2 # in pixels 
         print("self.img_w, self.img_h: ", self.img_w, self.img_h)
 
         self.tile_width, self.tile_height = 3840, 2160 # in pixels, defined in TilingFrame
